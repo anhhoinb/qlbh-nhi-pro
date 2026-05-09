7b18234e669931ef78b1fe0837efe173b55eb9dd
Nhi Pro <admin@nhipro.com>
2026-05-09 23:08:32 +0700

    remove auth

--- components/AuthGuard.tsx
+++ components/AuthGuard.tsx
@@ -1,89 +1,10 @@
 "use client";
 
-import {
-  onAuthStateChanged,
-} from "firebase/auth";
-
-import {
-  auth,
-} from "@/lib/firebase";
-
-import {
-  useRouter,
-  usePathname,
-} from "next/navigation";
-
-import {
-  useEffect,
-  useState,
-} from "react";
-
 export default function AuthGuard({
   children,
 }: {
   children: React.ReactNode;
 }) {
-
-  const router = useRouter();
-
-  const pathname =
-    usePathname();
-
-  const [loading, setLoading] =
-    useState(true);
-
-  // các trang không cần login
-  const publicRoutes = [
-    "/login",
-    "/products",
-    "/customers",
-    "/orders",
-  ];
-
-  useEffect(() => {
-
-    const unsubscribe =
-      onAuthStateChanged(
-        auth,
-        (user) => {
-
-          // route public
-          if (
-            publicRoutes.includes(
-              pathname
-            )
-          ) {
-
-            setLoading(false);
-
-            return;
-          }
-
-          // chưa login
-          if (!user) {
-
-            router.push("/login");
-
-            return;
-          }
-
-          setLoading(false);
-        }
-      );
-
-    return () => unsubscribe();
-
-  }, [pathname]);
-
-  if (loading) {
-
-    return (
-      <div className="p-10 text-2xl">
-        Đang tải...
-      </div>
-    );
-
-  }
 
   return <>{children}</>;
 }
